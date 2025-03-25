94b0abcb9f6efaa51c904ca45160f923b86f685d
Dev750wala <sadisatsowaladev1@gmail.com>
2025-03-25 15:12:10 +0530

    transferer list

---
+++ src/components/UserForm.tsx
@@ -0,0 +1,170 @@
+import React from "react";
+import { User } from "@/interface";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+const UserForm = ({
+  data,
+  setData,
+  isEditing,
+  setIsEditing,
+  form,
+  setForm,
+}: {
+  data: User[];
+  setData: React.Dispatch<React.SetStateAction<User[]>>;
+  isEditing: boolean;
+  setIsEditing: React.Dispatch<React.SetStateAction<boolean>>;
+  form: User;
+  setForm: React.Dispatch<React.SetStateAction<User>>;
+}) => {
+  function handleChangeForm(event: React.ChangeEvent<HTMLInputElement>) {
+    console.log("Form id is: ", form.id || "null");
+
+    setForm((prev) => {
+      return {
+        ...prev,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  function handleSaveUser(e: any) {
+    if (
+      form.username.length === 0 ||
+      form.fullName.length === 0 ||
+      form.city.length === 0 ||
+      form.age < 2
+    ) {
+      e.preventDefault();
+      alert("All the fields need to be filled up.");
+      return;
+    }
+
+    if (isEditing) {
+      const userIndex = data.findIndex((user: User) => {
+        return user.id === form.id;
+      });
+
+      let dataCopy = [...data];
+      dataCopy.splice(userIndex, 1, form);
+
+      setData(dataCopy);
+    } else {
+      setData((prev) => {
+        return [...prev, { ...form, id: Date.now().toString() }];
+      });
+    }
+    console.log(data);
+    setIsEditing(false);
+    setForm({
+      id: "",
+      age: 0,
+      city: "",
+      fullName: "",
+      username: "",
+      side: "left",
+    });
+  }
+
+  return (
+    <>
+      <div className="bg-white rounded-2xl border-blue-950 border-2 min-w-fit min-h-fit p-5 gap-3 flex flex-col justify-start">
+        <div className="flex flex-col gap-1 justify-start items-start">
+          <label htmlFor="username">Username</label>
+          <Input
+            required
+            type="text"
+            value={form.username}
+            name="username"
+            onChange={handleChangeForm}
+          />
+        </div>
+        <div className="flex flex-col gap-1 justify-start items-start">
+          <label htmlFor="fullName">Full Name</label>
+          <Input
+            required
+            type="text"
+            value={form.fullName}
+            name="fullName"
+            onChange={handleChangeForm}
+          />
+        </div>
+        <div className="flex flex-col gap-1 justify-start items-start">
+          <label htmlFor="city">City</label>
+          <Input
+            required
+            type="text"
+            value={form.city}
+            name="city"
+            onChange={handleChangeForm}
+          />
+        </div>
+        <div className="flex flex-col gap-1 justify-start items-start">
+          <label htmlFor="age">Age</label>
+          <Input
+            required
+            type="number"
+            value={form.age}
+            min={2}
+            name="age"
+            onChange={handleChangeForm}
+          />
+        </div>
+        <Button onClick={handleSaveUser} variant={"ghost"}>
+          {!isEditing ? "Add User" : "Update User"}
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default UserForm;
+
+// const InputComponent = ({
+//   type,
+//   value,
+//   min,
+//   name,
+//   onChange,
+// }: {
+//   type: "number" | "string";
+//   value: string | number;
+//   min?: number;
+//   name: string;
+//   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+// }) => {
+//   return (
+//     <div className="flex flex-col gap-1 justify-start items-start">
+//       <label htmlFor="age">Age</label>
+//       <Input
+//         required
+//         type="number"
+//         value={value}
+//         min={2}
+//         name="age"
+//         onChange={handleChangeForm}
+//       />
+//     </div>
+//   );
+// };
+
+interface InputInterface {
+  type: "number" | "string";
+  value: string | number;
+  min?: number;
+  name: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const InputComponent = (prop: InputInterface) => {
+  return (
+    <div className="flex flex-col gap-1 justify-start items-start">
+      <label htmlFor="age">Age</label>
+      <Input
+        required
+        {...prop}
+      />
+    </div>
+  );
+};
